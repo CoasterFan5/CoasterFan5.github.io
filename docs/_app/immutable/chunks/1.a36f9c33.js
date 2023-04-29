27b5432d08778397974038415fbaf7afce4524ce
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-04-28 20:50:34 -0400

    new build maybe

--- docs/_app/immutable/chunks/1.a36f9c33.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.701e8575.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1.d45b75ac.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.347f323d.js";export{t as component};
