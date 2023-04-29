14826b48d07d7389fe47c1cfa9ab2ba1abeefe16
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-04-28 20:41:01 -0400

    Start of home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<slot/>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		background: rgb(15, 15, 15);
+		color: rgb(228, 228, 228);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,158 @@
+<script>
+	import Sidebar from "$lib/sidebar.svelte";
+	import { positions } from "$lib/positions.js";
+	import { onMount } from "svelte";
+
+	let steps = 100;
+	let posCopy = JSON.parse(JSON.stringify(positions));
+	let dots = posCopy["name"];
+	let canvas;
+	let canvasHeight = 0;
+	let canvasWidth = 0;
+	let centerx = 0;
+	let centery = 0;
+	let squareRadius = 0;
+	let fadingTo = "name";
+	let ctx;
+	let frameRate = 60;
+
+	$: trueCanvasHeight = canvasHeight;
+	$: trueCanvasWidth = canvasWidth;
+	$: squareRadius = Math.floor(Math.min(canvasWidth/2, canvasHeight/2));
+	$: centerx = Math.floor(canvasWidth/2);
+	$: centery = Math.floor(canvasHeight/2);
+	
+
+	$: console.log(canvasHeight, canvasWidth)
+	
+
+	onMount(() => {
+		setInterval(() => {
+			fadeCanvas();
+		}, 1000/frameRate)
+		if(!canvas) return;
+		ctx = canvas.getContext("2d");
+	})
+	
+	let fadeCanvas = () => {
+
+
+		if(!canvas || !ctx) return;
+		//find a nice square centered on the canvas
+		
+		
+		canvas.height = canvasHeight;
+		canvas.width = canvasWidth;
+		let bottomx = centerx-squareRadius;
+		let topx = centerx+squareRadius;
+		let bottomy = centery-squareRadius;
+		let topy = centery+squareRadius;
+		let xstep = (topx-bottomx)/100;
+		let ystep = (topy-bottomy)/100;
+		dots.forEach((dot, index) => {
+			//get the dots position and the position it should be in
+			
+			if(!positions[fadingTo][index]) {
+				let newDot = {x: 0, y: 0};
+				newDot.x = Math.floor(Math.random()*100);
+				newDot.y = Math.floor(Math.random()*100);
+				newDot.opacity = 0;
+				positions[fadingTo].push(newDot);
+			}
+			
+			dot.opacity += (positions[fadingTo][index].opacity - dot.opacity)/10;
+			dot.x += ((positions[fadingTo][index].x) - dot.x)/frameRate;
+			dot.y += ((positions[fadingTo][index].y) - dot.y)/frameRate;
+			//add variation of 5-10 px to the position
+			
+			//draw the dot
+			ctx.fillStyle = `rgba(5, 129, 232, ${dot.opacity})`;
+			ctx.global = dot.opacity;
+			let dotx = dot.x * xstep + bottomx;
+			let doty = dot.y * ystep + bottomy;
+			ctx.moveTo(dotx, doty);
+			ctx.arc(dotx, doty, 5, 0, 2*Math.PI);
+		})
+		ctx.fill();
+	}
+
+	let setFadingTo = (name) => {
+		console.log(name)
+		fadingTo = name;
+	}
+</script>
+
+
+
+<div class="wrap">
+	<div class="sidebarwrap">
+		<a href="/" on:mouseenter={() => setFadingTo("name")}>CoasterFan5</a>
+		<hr>
+		<a href="/about" on:mouseenter={() => setFadingTo("about")}>About</a>
+		<a href="/projects" on:mouseenter={() => setFadingTo("projects")}>Projects</a>
+		<a href="/contact" on:mouseenter={() => setFadingTo("contact")}>Contact</a>
+	</div>
+	<div class="dotHolder" bind:clientHeight={canvasHeight} bind:clientWidth={canvasWidth}>
+		<canvas bind:this={canvas}> 
+			
+	</div>
+</div>
+
+
+
+<style>
+	
+	.wrap {
+		display: flex;
+		width: 100vw;
+		height: 100vh;
+		flex-direction: row;
+	}
+	.dotHolder {
+		width: 100%;
+		flex-grow: 2;
+		height: 100%;
+		display: flex;
+		background: rgba(255, 255, 255, 0.1);
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.sidebarwrap {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		width: 200px;
+		text-align: center;
+		background: rgba(0, 0, 0, 0.1);
+	}
+	.sidebarwrap hr {
+		width: 70%;
+		border: none;
+		border-bottom: 1px solid rgba(228, 228, 228, 0.1);
+		
+	}
+	.sidebarwrap a {
+		text-decoration: none;
+		background-image: linear-gradient(90deg, #0581E8 75%, white 50%);
+		background-position: 100%;
+		background-size: 400%;
+		background-clip: text;
+		-webkit-background-clip: text;
+		color: transparent;
+		transition: background-position 0.5s;
+		font-size: 1.5rem;
+
+	}
+	.sidebarwrap a:hover {
+		text-decoration: none;
+		background-image: linear-gradient(90deg, #0581E8 75%, white 50%);
+		background-position: 0%;
+		background-size: 400%;
+		
+		color: transparent;
+		transition: background-position 1s;
+	}
+</style>
