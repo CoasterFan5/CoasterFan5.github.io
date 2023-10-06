925b0e9b1e5d93631dca25d6b118001104150f4b
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-10-05 20:46:48 -0400

    Better website!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,56 +1,13 @@
-
-<script>
-	import Sidebar from "$lib/Sidebar.svelte";
-</script>
-
-<div class="wrap">
-	<div class="secondWrap">
-		<Sidebar/>
-		<div class="content">
-			<slot>
-
-			</slot>
-		</div>
-	</div>
-</div>
-
+<slot/>
 
 <style>
 	:global(body) {
 		margin: 0;
 		padding: 0;
-		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		font-family: sans-serif;
+		background: #f1f1f1;
 	}
-	.content {
-		width: 100%;
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.wrap {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		width: 100%;
-		background-image: url("/bg.svg");
-		background-size: cover;
-		background-position: center;
-	}
-	.secondWrap {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		width: 100%;
-		background: rgba(13, 13, 13, 0.5);
-		backdrop-filter: blur(200px);
-		background-size: cover;
-		background-position: center;	
+	:global(html) {
+		scroll-behavior:smooth
 	}
 </style>
